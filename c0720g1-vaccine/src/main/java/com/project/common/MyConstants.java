2812fa67bbe2a27a3e04b8abd09beaa47a9bdeba
pato <pato24193@gmail.com>
2021-04-25 12:37:31 +0700

    pato upload firebase

--- c0720g1-vaccine/src/main/java/com/project/common/MyConstants.java
+++ c0720g1-vaccine/src/main/java/com/project/common/MyConstants.java
@@ -2,10 +2,10 @@ package com.project.common;
 
 public class MyConstants {
     // Replace with your email here:
-    public static final String MY_EMAIL = "pato24193@gmail.com";
+    public static final String MY_EMAIL = "...@gmail.com";
 
     // Replace password!!
-    public static final String MY_PASSWORD = "Kiripi24193";
+    public static final String MY_PASSWORD = "...";
 
     // And receiver!
     public static final String MAIL_RECEIVER= "";
